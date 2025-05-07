b7c6b5e3a21e4a7d50be3263d61ec6e99823781d
Minater247 <nreed132@gmail.com>
2025-05-06 21:01:47 -0700

    Implement speech synthesis in explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,34 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let availableSpeechSynthesis = window.speechSynthesis.getVoices();
+  let voiceSelect = document.getElementById('voice-select');
+
+  availableSpeechSynthesis.forEach(voice => {
+    let option = document.createElement('option');
+    option.value = voice.name;
+    option.textContent = `${voice.name} (${voice.lang})`;
+    voiceSelect.appendChild(option);
+  })
+
+  let speakButton = document.querySelector('button');
+  let textContent = document.getElementById('text-to-speak');
+  let img = document.querySelector('img');
+
+  speakButton.addEventListener('click', () => {
+    let selectedVoice = voiceSelect.value;
+    let utterance = new SpeechSynthesisUtterance(textContent.value);
+    utterance.voice = availableSpeechSynthesis.find(voice => voice.name === selectedVoice);
+    window.speechSynthesis.speak(utterance);
+
+    utterance.addEventListener('start', () => {
+      img.src = 'assets/images/smiling-open.png';
+      img.alt = 'Smiling face';
+    });
+
+    utterance.addEventListener('end', () => {
+      img.src = 'assets/images/smiling.png';
+      img.alt = 'Smiling face';
+    });
+  });
 }
