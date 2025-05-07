e0035d69b28d3fc6d658c2ec6167fe97d36324e3
Minater247 <nreed132@gmail.com>
2025-05-06 20:40:05 -0700

    Add Javascript to the Horn page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+## Logistics
+**Lab Writer**: Nathan Reed
+
+Due to time constraints and general difficulty scheduling, I found it best to work solo on this project. I do not want to freeload on anyone else's work, as I was not able to find any viable time to meet up as a pair/group.
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,47 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  const slider = document.querySelector('[type="range"]');
+  const audio = document.querySelector('audio');
+  const icon = document.querySelector('#volume-controls > img');
+
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose > img');
+
+  const playButton = document.querySelector('button');
+
+  const jsConfetti = new JSConfetti();
+  
+  slider.addEventListener('input', function() {
+    audio.volume = this.value / 100;
+
+    if (this.value < 1) {
+      icon.src = 'assets/icons/volume-level-0.svg';
+      icon.alt = 'Volume Level 0';
+    } else if (this.value < 33) {
+      icon.src = 'assets/icons/volume-level-1.svg';
+      icon.alt = 'Volume Level 1';
+    } else if (this.value < 67) {
+      icon.src = 'assets/icons/volume-level-2.svg';
+      icon.alt = 'Volume Level 2';
+    } else {
+      icon.src = 'assets/icons/volume-level-3.svg';
+      icon.alt = 'Volume Level 3';
+    }
+  });
+
+  hornSelect.addEventListener('input', function() {
+    hornImage.src = 'assets/images/' + this.value + '.svg';
+    hornImage.alt = this.value;
+
+    audio.src = 'assets/audio/' + this.value + '.mp3';
+  });
+
+  playButton.addEventListener('click', function() {
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+    audio.play();
+  });
 }
